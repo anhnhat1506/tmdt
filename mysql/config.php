000ec6f86a739aba9df6e061d42cb7abfebc38b3
vuong nguyen <nmvuong92@gmaial.com>
2018-12-16 12:28:30 +0700

    mysql helper class

---
+++ mysql/config.php
@@ -0,0 +1,15 @@
+<?php
+ini_set( "display_errors", true );
+define( "DB_DSN", "mysql:host=localhost;dbname=latihan" );
+define( "DB_USERNAME", "root" );
+define( "DB_PASSWORD", "" );
+define('DB_CHARACSET', 'utf8');
+require_once ('Database.php');
+//require_once ('My_pagination.php');
+$db=new Database();
+//$pg=New My_pagination();
+function handleException( $exception ) {
+    echo  $exception->getMessage();
+}
+set_exception_handler( 'handleException' );
+?>
